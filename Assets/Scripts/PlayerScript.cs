c7bf1acaa7c47633ada9e29aa739bfb79db1ecf9
sorata1226 <cita16045@g.nihon-u.ac.jp>
2018-11-23 13:34:04 +0900

    first commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.speed = 1;//このようにスクリプトからNavMeshのプロパティを変更できる。
+        agent.destination = target.transform.position;
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
